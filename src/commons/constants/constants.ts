d999b3985f2bc47da177da1dc0766a9fd5315744
Sebastian Pucheta <seba.pucheta.17@gmail.com>
2025-01-02 09:04:10 -0300

    Add middleware to add default header to the response

---
+++ src/commons/constants/constants.ts
@@ -0,0 +1,6 @@
+export const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST,GET,PUT,PATCH.DELETE,OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+  'Content-Type': 'application/json'
+};
